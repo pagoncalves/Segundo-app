a8850e579a7df7eb218f85a968decb6fc271b7b2
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-03-13 10:17:16 -0300

    Alterada forma de exibição

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ class App extends React.Component {
     this.state = {
       nome: " ",
       msg: " ",
-      msg2: " ",
       status: "",
       n1: 0,
       n2: 0,
-      media: 0.0
+      media: 0.0,
+      passo:0
+      
     }
   }
 
   handleAperte = () => {
-    this.setState((state) => ({ msg: `Seja bem vindo ${state.nome}` }))
-    this.setState((state) => ({ msg2: `Insira suas notas: ` }))
+    this.setState((state) => ({     
+    passo:1   }))
   }
+
   handleInput = (event) => {
     const name = event.target.name
     this.setState({ [name]: event.target.value })
   }
 
   handleCalc = () => {
-    this.setState((state) => ({ media: `Sua média é ${((parseFloat(state.n1) + parseFloat(state.n2)) / 2).toFixed(1)}` }))
+    this.setState((state) => ({ media: `Sua média é ${((parseFloat(state.n1) + parseFloat(state.n2)) / 2).toFixed(1)}`,
+    passo:2 }))
     let media1 = (parseFloat(this.state.n1) + parseFloat(this.state.n2)) / 2;
     if (media1 >= 7) {
       this.setState((state) => ({ status: `Parabéns ${this.state.nome} você foi aprovado!` }))
     } else {
-      this.setState((state) => ({ status: `${this.state.nome} você foi reprovado(a)!` }))
+      this.setState((state) => ({ status: `${this.state.nome} você foi reprovado(a)!` }))  
     }
   }
 
@@ -39,7 +42,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="TCard">
-          <div className="Card">
+          {this.state.passo === 0 && <div className="Card">
 
             <h1>Olá!</h1>
             <h2>Digite seu nome:</h2>
@@ -47,13 +50,12 @@ class App extends React.Component {
             <p><button onClick={this.handleAperte}  >Aperte</button></p>
 
             <div className="RCard">Passo 1</div>
-          </div>
+          </div>}
 
-        
 
-         <div className="Card">
-          <p> {this.state.msg}</p>
-          <p>{this.state.msg2} </p>
+         {this.state.passo >= 1 && <div className="Card">
+          <p>Seja bem vindo {this.state.nome}</p>
+          <p>Insira suas notas: </p>
           <p>Nota 1:</p>
           <input name="n1" value={this.state.n1} onChange={this.handleInput}></input>
           <p>Nota 2:</p>
@@ -62,15 +64,15 @@ class App extends React.Component {
 
           <div className="RCard">Passo 2 </div>
 
-         </div>
+         </div>}
 
-         <div className="Card">
+         {this.state.passo >= 2 && <div className="Card">
           <h3>Média</h3>
           <p>{this.state.media}</p>
           <p>{this.state.status}</p>
 
           <div className="RCard">Resultado</div>
-         </div>
+         </div>}
          </div>
       </div>
       
