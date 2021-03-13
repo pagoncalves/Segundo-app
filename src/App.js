afe0cde6d176860b3857d5f4001043677f5e7c5e
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-03-13 09:01:23 -0300

    primeira versão

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: " ",
+      msg: " ",
+      msg2: " ",
+      status: "",
+      n1: 0,
+      n2: 0,
+      media: 0.0
+    }
+  }
+
+  handleAperte = () => {
+    this.setState((state) => ({ msg: `Seja bem vindo ${state.nome}` }))
+    this.setState((state) => ({ msg2: `Insira suas notas: ` }))
+  }
+  handleInput = (event) => {
+    const name = event.target.name
+    this.setState({ [name]: event.target.value })
+  }
+
+  handleCalc = () => {
+    this.setState((state) => ({ media: `Sua média é ${((parseFloat(state.n1) + parseFloat(state.n2)) / 2).toFixed(1)}` }))
+    let media1 = (parseFloat(this.state.n1) + parseFloat(this.state.n2)) / 2;
+    if (media1 >= 7) {
+      this.setState((state) => ({ status: `Parabéns ${this.state.nome} você foi aprovado!` }))
+    } else {
+      this.setState((state) => ({ status: `${this.state.nome} você foi reprovado(a)!` }))
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <div className="TCard">
+          <div className="Card">
+
+            <h1>Olá!</h1>
+            <h2>Digite seu nome:</h2>
+            <p><input name="nome" value={this.state.nome} onChange={this.handleInput} ></input> </p>
+            <p><button onClick={this.handleAperte}  >Aperte</button></p>
+
+            <div className="RCard">Passo 1</div>
+          </div>
+
+        </div>
+
+        <div className="Card">
+          <p> {this.state.msg}</p>
+          <p>{this.state.msg2} </p>
+          <p>Nota 1:</p>
+          <input name="n1" value={this.state.n1} onChange={this.handleInput}></input>
+          <p>Nota 2:</p>
+          <input name="n2" value={this.state.n2} onChange={this.handleInput}></input>
+          <p> <button onClick={this.handleCalc}>Calcular média:</button></p>
+
+          <div className="RCard">Passo 2 </div>
+
+        </div>
+
+        <div className="Card">
+          <h3>Resultado</h3>
+          <p>{this.state.media}</p>
+          <p>{this.state.status}</p>
+
+          <div className="RCard">Passo 3</div>
+        </div>
+
+      </div>
+      
+    );
+  }
 }
+
 
 export default App;
