80c4052e44d7b3bd1ac56d5c8fe1a3b12b41dedc
Patricia Goncalves <patriciagpereira89@gmail.com>
2021-03-13 10:34:13 -0300

    Documento formatado

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ class App extends React.Component {
       n1: 0,
       n2: 0,
       media: 0.0,
-      passo:0
-      
+      passo: 0
+
     }
   }
 
   handleAperte = () => {
-    this.setState((state) => ({     
-    passo:1   }))
+    this.setState((state) => ({
+      passo: 1
+    }))
   }
 
   handleInput = (event) => {
@@ -26,13 +27,15 @@ class App extends React.Component {
   }
 
   handleCalc = () => {
-    this.setState((state) => ({ media: `Sua média é ${((parseFloat(state.n1) + parseFloat(state.n2)) / 2).toFixed(1)}`,
-    passo:2 }))
+    this.setState((state) => ({
+      media: `Sua média é ${((parseFloat(state.n1) + parseFloat(state.n2)) / 2).toFixed(1)}`,
+      passo: 2
+    }))
     let media1 = (parseFloat(this.state.n1) + parseFloat(this.state.n2)) / 2;
     if (media1 >= 7) {
       this.setState((state) => ({ status: `Parabéns ${this.state.nome} você foi aprovado!` }))
     } else {
-      this.setState((state) => ({ status: `${this.state.nome} você foi reprovado(a)!` }))  
+      this.setState((state) => ({ status: `${this.state.nome} você foi reprovado(a)!` }))
     }
   }
 
@@ -52,27 +55,27 @@ class App extends React.Component {
           </div>}
 
 
-         {this.state.passo >= 1 && <div className="Card">
-          <p>Seja bem vindo {this.state.nome}</p>
-          <p>Insira suas notas: </p>
-          <p>Nota 1:</p>
-          <input name="n1" value={this.state.n1} onChange={this.handleInput}></input>
-          <p>Nota 2:</p>
-          <input name="n2" value={this.state.n2} onChange={this.handleInput}></input>
-          <p> <button onClick={this.handleCalc}>Calcular média:</button></p>
+          {this.state.passo >= 1 && <div className="Card">
+            <p>Seja bem vindo {this.state.nome}</p>
+            <p>Insira suas notas: </p>
+            <p>Nota 1:</p>
+            <input name="n1" value={this.state.n1} onChange={this.handleInput}></input>
+            <p>Nota 2:</p>
+            <input name="n2" value={this.state.n2} onChange={this.handleInput}></input>
+            <p> <button onClick={this.handleCalc}>Calcular média:</button></p>
 
-          <div className="RCard">Passo 2 </div>
+            <div className="RCard">Passo 2 </div>
 
-         </div>}
+          </div>}
 
-         {this.state.passo >= 2 && <div className="Card">
-          <h3>Média</h3>
-          <p>{this.state.media}</p>
-          <p>{this.state.status}</p>
+          {this.state.passo >= 2 && <div className="Card">
+            <h3>Média</h3>
+            <p>{this.state.media}</p>
+            <p>{this.state.status}</p>
 
-          <div className="RCard">Resultado</div>
-         </div>}
-         </div>
+            <div className="RCard">Resultado</div>
+          </div>}
+        </div>
       </div>
     );
   }
